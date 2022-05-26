0e15fdda5b1c6bdb30c3a18dd3ce1856c896ef6e
IAFEnvoy <tanksheng545@126.com>
2022-05-26 23:07:09 +0800

    更加紧凑

--- app/src/main/java/tmbpg/czmd5/Game/GameRunner.java
+++ app/src/main/java/tmbpg/czmd5/Game/GameRunner.java
@@ -39,36 +39,35 @@ public class GameRunner {
    * @return 游戏是否仍在运行，false表示计算结束
    */
   private boolean tick() {
+    if (peoples.size() == 1)
+      throw new RuntimeException("就一个人了你玩个锤子");
     tick++;
-    // LogUtil.log("第" + tick + "回合");
     List<People> dead = new ArrayList<>();
-    for (People p : peoples) {
-      p.tickEffects();
-      if (p.shouldAttack()) {
-        People target = peoples.get(p.nextInt(peoples.size()));
-        if (target.getName().equals(p.getName()))
-          continue;
-        int damage = p.getDamageAmount(target);
-        target.damage(damage);
-        LogUtil.log(String.format("第%d回合：%s 攻击了 %s ，血量%d->%d", tick, p.getName(), target.getName(),
-            target.getHp() + damage, target.getHp()), TextColor.YELLOW);
-        for (SkillBase skill : p.getSkills())
-          if (p.nextInt(skill.getTriggerProb()) == 0) {
-            target.damage(skill.getDamage(target));
-            LogUtil.log(String.format("%s 额外触发技能 %s ，血量%d->%d", p.getName(), skill.getName(),
-                target.getHp() + skill.getDamage(target), target.getHp()), TextColor.CYAN);
-            for (EffectBase effect : skill.getEffects()) {
-              target.addEffect(effect);
-              LogUtil.log(String.format("%s 获得效果： %s ，持续%d秒", target.getName(), effect.getName(), effect.getTime()),
-                  TextColor.MAGENTA);
-            }
-          }
-        if (target.isDead()) {
-          LogUtil.log(String.format("%s 死了", target.getName()), TextColor.RED);
-          dead.add(target);
+    People source = peoples.get(random.nextInt(peoples.size()));
+    People target = peoples.get(random.nextInt(peoples.size()));
+    while (target.getName().equals(source.getName()))
+      target = peoples.get(random.nextInt(peoples.size()));
+    int damage = source.getDamageAmount(target);
+    target.damage(damage);
+    LogUtil.log(String.format("第%d回合：%s 攻击了 %s ，血量%d->%d", tick, source.getName(), target.getName(),
+        target.getHp() + damage, target.getHp()), TextColor.YELLOW);
+    for (SkillBase skill : source.getSkills())
+      if (source.nextInt(skill.getTriggerProb()) == 0) {
+        target.damage(skill.getDamage(target));
+        LogUtil.log(String.format("%s 额外触发技能 %s ，血量%d->%d", source.getName(), skill.getName(),
+            target.getHp() + skill.getDamage(target), target.getHp()), TextColor.CYAN);
+        for (EffectBase effect : skill.getEffects()) {
+          target.addEffect(effect);
+          LogUtil.log(String.format("%s 获得效果： %s ，持续%d秒", target.getName(), effect.getName(), effect.getTime()),
+              TextColor.MAGENTA);
         }
       }
+    if (target.isDead()) {
+      LogUtil.log(String.format("%s 死了", target.getName()), TextColor.RED);
+      dead.add(target);
     }
+    for (People p : peoples)
+      p.tickEffects();
     peoples.removeAll(dead);
     return peoples.size() > 1;
   }
